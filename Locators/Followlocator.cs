886989d702df8f510a0986ea5cdbd4caaec3a176
mg-storm <61591798+mg-storm@users.noreply.github.com>
2020-06-11 21:22:30 -0500

    Update Followlocator.cs

--- Locators/Followlocator.cs
+++ Locators/Followlocator.cs
@@ -19,7 +19,7 @@ namespace ProjectFacebook.Locators
             driver = Driver;
             wait = Wait;
         }
-         pubic IWebElement TextBuscar => driver.FindElement(By.XPath("//input[contains(@name,'q')]"));
+         public IWebElement TextBuscar => driver.FindElement(By.XPath("//input[contains(@name,'q')]"));
          public IWebElement LinkCova => driver.FindElement(By.XPath("//a[contains(text(),'Cova LoL')] "));
          public IWebElement DaleLike => driver.FindElement(By.XPath("//button[contains(text(),'Me gusta')]"));
          public IWebElement SeleccionaCmt => driver.FindElement(By.XPath("/html[1]/body[1]/div[13]/div[2]/div[1]/div[1]/div[1]/div[2]/div[1]/div[2]/div[2]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/form[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]"));
