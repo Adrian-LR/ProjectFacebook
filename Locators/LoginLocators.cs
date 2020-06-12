6cb42279c4574b320e3dd54ff02dc96d8c45a62a
Adrian-LR <adiran.21@gmail.com>
2020-06-12 08:10:00 -0500

    Se agregaron locators faltantes

--- Locators/LoginLocators.cs
+++ Locators/LoginLocators.cs
@@ -20,9 +20,11 @@ namespace ProjectFacebook.Locators
             driver = Driver;
             wait = Wait;
         }
-        public IWebElement UserBox => driver.FindElementByCssSelector("#email");
-        public IWebElement PwdBox => driver.FindElementByCssSelector("#pass");
-        public IWebElement LoginButton => driver.FindElementByCssSelector("#loginbutton");
+        public By UserBox = By.CssSelector("#email");
+        public By UserBox2 = By.CssSelector(".inputtext.login_form_input_box#email");
+        public By PwdBox = By.CssSelector("#pass");
+        public By PwdBox2 = By.CssSelector(".inputtext.login_form_input_box#pass");
+        public By LoginButton = By.CssSelector("#loginbutton");
 
     }
 }
