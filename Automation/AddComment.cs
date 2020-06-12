e74a5cb3b63036704715ccdc6e7d5fdddb10bcc0
Alberto <alberto.gutierrez@e-quallity.net>
2020-06-12 08:57:04 -0500

    Versión Con chat completo

--- Automation/AddComment.cs
+++ Automation/AddComment.cs
@@ -7,6 +7,7 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Remote;
 using OpenQA.Selenium.Support.UI;
 using ProjectFacebook.Locators;
+using System.Threading;
 
 
 namespace ProjectFacebook.Automation
@@ -17,6 +18,7 @@ namespace ProjectFacebook.Automation
         private WebDriverWait wait;
         
         LoginLocators loginLoc;
+        HomeLocators homeLoc;
 
         public AddComment(RemoteWebDriver Driver, WebDriverWait Wait)
         {
@@ -58,6 +60,74 @@ namespace ProjectFacebook.Automation
                 driver.FindElement(loginLoc.LoginButton).Click();
             }
 
+        }
+
+        public bool mandarMensaje(RemoteWebDriver driver)
+        {
+            var mensaje = false;
+            try
+            {
+                homeLoc = new HomeLocators(driver, wait);
+                Thread.Sleep(7000);
+                if (IsElementPresent(homeLoc.btnbuscarChat))
+                {
+                    driver.FindElement(homeLoc.btnbuscarChat).Click();
+                    Thread.Sleep(5000);
+                }
+                else if (IsElementPresent(homeLoc.btnbuscarChat1))
+                {
+                    driver.FindElement(homeLoc.btnbuscarChat1).Click();
+                    Thread.Sleep(5000);
+                }
+                if (IsElementPresent(homeLoc.txtBuscarChat))
+                {
+                    driver.FindElement(homeLoc.txtBuscarChat).Click();
+                    Thread.Sleep(2000);
+
+                }
+                else if (IsElementPresent(homeLoc.txtBuscarChat1))
+                {
+                    driver.FindElement(homeLoc.txtBuscarChat1).Click();
+                    Thread.Sleep(2000);
+                }
+                if (IsElementPresent(homeLoc.txtBuscarChat))
+                {
+                    driver.FindElement(homeLoc.txtBuscarChat).SendKeys("Oscar Cova");
+                    Thread.Sleep(5000);
+                }
+                else if (IsElementPresent(homeLoc.txtBuscarChat1))
+                {
+                    driver.FindElement(homeLoc.txtBuscarChat1).SendKeys("Oscar Cova");
+                    Thread.Sleep(5000);
+                }
+                if (IsElementPresent(homeLoc.seleccionaCova))
+                {
+                    driver.FindElement(homeLoc.seleccionaCova).Click();
+                    Thread.Sleep(2000);
+
+                }
+                else if (IsElementPresent(homeLoc.seleccionaCova1))
+                {
+                    driver.FindElement(homeLoc.seleccionaCova1).Click();
+                    Thread.Sleep(2000);
+                }
+                if (IsElementPresent(homeLoc.txtMensajeCova))
+                {
+                    driver.FindElement(homeLoc.txtMensajeCova).SendKeys("Hola");
+                    Thread.Sleep(2000);
+                }
+                else if (IsElementPresent(homeLoc.txtMensajeCova1))
+                {
+                    driver.FindElement(homeLoc.txtMensajeCova1).SendKeys("Hola");
+                    Thread.Sleep(2000);
+                }
+               
+                return mensaje;
+            }
+            catch (Exception e)
+            {              
+                return false;
+            }
         }
 
     }
